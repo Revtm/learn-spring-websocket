149dc68185be00f4a46e014719da107f6d3b98b9
Revtm <kykylizaro98@gmail.com>
2024-01-30 16:45:38 +0700

    add sendToUser

--- src/main/java/com/revtm/websocketchat/config/WebSocketConfig.java
+++ src/main/java/com/revtm/websocketchat/config/WebSocketConfig.java
@@ -1,23 +1,68 @@
 package com.revtm.websocketchat.config;
 
+import jakarta.servlet.http.HttpSession;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.http.server.ServletServerHttpRequest;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.server.support.DefaultHandshakeHandler;
+
+import java.util.Map;
 
 @Configuration
 @EnableWebSocketMessageBroker
+@Slf4j
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
-        registry.enableSimpleBroker("/topic");
+        registry.enableSimpleBroker("/topic/", "/queue/");
         registry.setApplicationDestinationPrefixes("/app");
     }
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-       registry.addEndpoint("/chat");
-       registry.addEndpoint("/chat").withSockJS();
+        registry.addEndpoint("/chat");
+        registry.addEndpoint("/chat").withSockJS();
+
+        registry.addEndpoint("/private-chat")
+                .setHandshakeHandler(new DefaultHandshakeHandler(){
+                    public boolean beforeHandshake(
+                            ServerHttpRequest request,
+                            ServerHttpResponse response,
+                            WebSocketHandler wsHandler,
+                            Map attributes
+                    ){
+                        if(request instanceof ServletServerHttpRequest){
+                            ServletServerHttpRequest servletServerHttpRequest = (ServletServerHttpRequest) request;
+                            HttpSession session = servletServerHttpRequest.getServletRequest()
+                                    .getSession();
+                            attributes.put("sessionId", session.getId());
+                        }
+                        return true;
+                    }
+                });
+        registry.addEndpoint("/private-chat").setHandshakeHandler(new DefaultHandshakeHandler(){
+            public boolean beforeHandshake(
+                    ServerHttpRequest request,
+                    ServerHttpResponse response,
+                    WebSocketHandler wsHandler,
+                    Map attributes
+            ){
+                if(request instanceof ServletServerHttpRequest){
+                    ServletServerHttpRequest servletServerHttpRequest = (ServletServerHttpRequest) request;
+                    HttpSession session = servletServerHttpRequest.getServletRequest()
+                            .getSession();
+                    log.info(session.getId());
+                    attributes.put("sessionId", session.getId());
+                }
+                return true;
+            }
+        }).withSockJS();
     }
 }
--- src/main/java/com/revtm/websocketchat/controller/ChatController.java
+++ src/main/java/com/revtm/websocketchat/controller/ChatController.java
@@ -1,19 +1,46 @@
 package com.revtm.websocketchat.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.revtm.websocketchat.dto.InputMessageDto;
 import com.revtm.websocketchat.dto.OutputMessageDto;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.messaging.simp.annotation.SendToUser;
 import org.springframework.stereotype.Controller;
 
+import java.security.Principal;
 import java.time.LocalDateTime;
 
 @Controller
 public class ChatController {
 
+    private final SimpMessageSendingOperations simpMessageSendingOperations;
+    private final ObjectMapper objectMapper;
+    @Autowired
+    public ChatController(SimpMessageSendingOperations simpMessageSendingOperations, ObjectMapper objectMapper) {
+        this.simpMessageSendingOperations = simpMessageSendingOperations;
+        this.objectMapper = objectMapper;
+    }
+
     @MessageMapping("/chat")
     @SendTo("/topic/messages")
-    public OutputMessageDto send(InputMessageDto inputMessage){
+    public OutputMessageDto send(InputMessageDto inputMessage) throws Exception {
+        String time = LocalDateTime.now().toString();
+        return OutputMessageDto.builder()
+                .from(inputMessage.getFrom())
+                .msg(inputMessage.getMsg())
+                .time(time)
+                .build();
+    }
+
+    @MessageMapping("/private-chat")
+    @SendToUser("/queue/reply")
+    public OutputMessageDto sendSpecific(@Payload String message, Principal principal) throws Exception {
+        InputMessageDto inputMessage = objectMapper.readValue(message, InputMessageDto.class);
         String time = LocalDateTime.now().toString();
         return OutputMessageDto.builder()
                 .from(inputMessage.getFrom())
